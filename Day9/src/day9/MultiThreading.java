b6072d30e5b187f9fba68e86bee9f6fc6e28d1c6
jawad <jawadusaid11@gmail.com>
2022-02-17 23:37:38 +0530

    MultiThreading, priority of thread, Threat Id

---
+++ Day9/src/day9/MultiThreading.java
@@ -0,0 +1,46 @@
+package day9;
+
+class bike extends Thread {
+	public void run() {
+	System.out.println("the thread id no. for the bike class is: " + Thread.currentThread().getId());
+	}
+}
+
+class cycles extends Thread {
+	public void run() {
+		
+	
+	System.out.println("the thread id no. for the cycles class is: " + Thread.currentThread().getId());
+}
+}
+
+class scooter extends Thread {
+	public void run() {
+	System.out.println("the thread id no. for the scooter class is: " + Thread.currentThread().getId());
+	}
+}
+public class MultiThreading implements Runnable {
+	public void run() {
+		System.out.println("the thread id no. for the MULTITHREADING class is: " + Thread.currentThread().getId());
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<5;i++) {
+		bike b = new bike();
+		b.setPriority(Thread.NORM_PRIORITY);
+		b.start();
+		}
+		cycles c = new cycles();
+		c.setPriority(Thread.MAX_PRIORITY);
+		c.start();
+		scooter s = new scooter();
+		s.start();
+		MultiThreading m = new MultiThreading();
+		Thread t = new Thread(m);
+		t.setPriority(Thread.MAX_PRIORITY);
+		System.out.println("Id no. for main is: "+ Thread.currentThread().getId());
+		t.start();
+	}
+
+}
